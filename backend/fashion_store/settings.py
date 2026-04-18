4fb37271e7527d7a96df808ea295903d92146af4
Elue Ezekiel <elueezekiel2005@gmail.com>
2026-04-18 21:41:37 +0100

    database changed to postgres

--- backend/fashion_store/settings.py
+++ backend/fashion_store/settings.py
@@ -4,6 +4,7 @@ Django settings for fashion_store project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,10 +59,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fashion_store.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
